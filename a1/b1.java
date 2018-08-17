06d1a394bf06ab52d5be9c2041327afa4021aaa2
wuweiping <wuweiping@astline.cn>
2018-08-17 14:17:59 +0800

    在master分支上向b1.java添加第三句

--- a1/b1.java
+++ a1/b1.java
@@ -2,5 +2,6 @@ public  class Demo{
 	public static void main(String agrs[]){
 		System.out.println("this is a java file in branch1");
 		System.out.println("在a1分支上添加的词句");
+		System.out.println("在master分支上添加的一句");
 	}
 }
